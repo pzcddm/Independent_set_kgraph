371abf4bc5e58a88c668bc51cb4fe72a0ba0bf41
pzcddm <786858269@qq.com>
2022-05-20 21:22:54 +0800

    After Make

---
+++ CMakeFiles/kgraph.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ZhencanPeng/Independent_set_kgraph/kgraph.cpp" "/home/ZhencanPeng/Independent_set_kgraph/CMakeFiles/kgraph.dir/kgraph.cpp.o"
+  "/home/ZhencanPeng/Independent_set_kgraph/metric.cpp" "/home/ZhencanPeng/Independent_set_kgraph/CMakeFiles/kgraph.dir/metric.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
